d01a1eeea61e5ecc79f37413a27a3bab6c0dd64e
HarryZen <358067541@qq.com>
2020-04-22 16:49:34 +0800

    change the "Image.thumbnail" to "Image.resize()"

--- convert2thumbnail.py
+++ convert2thumbnail.py
@@ -23,28 +23,32 @@ def convert_img(directory):
         des_directory = directory.dir + '/thumbnail'
         if not os.path.exists(des_directory):
             os.mkdir(des_directory)
-        filename = directory.dir + '/*'
-        img_list = glob.glob(filename)[0:-1]
-        for img in img_list:
+        filename = directory.dir + '/*[.jpg|.jpeg|.png]'
+        img_list = glob.glob(filename)
+        for index, img in enumerate(img_list):
+            img.replace('\\','/')
             thumbnail_img_name = des_directory + \
-                '/' + img[img.rfind('/')+1: img.rfind('.')] + '_tn.jpg'
-            thumbnail_img = convert(img, thumbnail_img_name)
+                '/' + str(index) + '_tn.jpg'
+            print(img, thumbnail_img_name)
+            convert(img, thumbnail_img_name)
         result = tkinter.messagebox.showinfo(title='转换完成', message='转换完成')
-        # if result:
-        #     os.system('start explorer'+des_directory) 
+        if result:
+            top.destroy()
    
 def convert(img, name):
     ori_image = Image.open(img)
-    ori_image.thumbnail((128,128), Image.ANTIALIAS)
-    ori_image.save(name)
+    out_image = ori_image.resize((640,480), Image.ANTIALIAS)
+    out_image.save(name)
 
 
 top = tkinter.Tk()
 top.title('女王专用图片转换工具，比较丑多见谅')
 top.geometry('640x480')
 source_directory = Directory()
-preview_canvas = tkinter.Canvas(top, bg='white', bd=5, height=300, width='300')
-preview_canvas.pack(anchor = 'center')
+# preview_canvas = tkinter.Canvas(top, bg='white', bd=5, height=300, width=300)
+# canvas_img = tkinter.PhotoImage(file='./flower.jpg')
+# preview_canvas.create_image(100, 100, image=canvas_img)
+# preview_canvas.pack(anchor = 'center')
 source_label = tkinter.Label(top)
 source_label.pack()
 source_button = tkinter.Button(top, text='选择文件夹', command=source_directory.get_dir)
